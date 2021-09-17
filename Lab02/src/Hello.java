3d8504f3272bf26bfa00726e1a519b92754cdef1
Adversus <Adversus@DESKTOP-PDPLB1R>
2021-09-16 21:17:58 -0700

    Steven Symansiv in line 9

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,8 +6,8 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Rafaela Lomboy.");
-		System.out.println("SacStateID - This line was added by Keaton Wakefield.");
-		System.out.println("SacStateID - This line was added by Thanh Le.");
+		System.out.println("SacStateID - This line was added by Steven Symansiv.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
