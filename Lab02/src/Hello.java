77e5701c07c8e535c4f9d4997ae76eab061742ee
Mark <Mark@MARKSASUSLAPTOP>
2021-09-21 13:40:44 -0700

    commit

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,11 +6,11 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Rafaela Lomboy.");
+		System.out.println("SacStateID - This line was added by Mark Vorobets.");
 		System.out.println("SacStateID - This line was added by Steven Symansiv.");
 		System.out.println("SacStateID - This line was added by Kadien Tran.");
 		System.out.println("SacStateID - This line was added by Keaton Wakefield.");
 		System.out.println("SacStateID - This line was added by Thanh Le.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
